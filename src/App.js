e1abb4dbce0190bc83d013aa25a4431fb027bad8
rojadeshetty <rojadeshetty@gmail.com>
2024-03-20 23:18:25 +0530

    added project files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>About Disney RRR</h1>
+      <p>Disney is an American multinational entertainment company known for its movies, television shows, theme parks, and merchandise. It was founded in 1923 by Walt Disney and his brother Roy Disney. Disney has produced many popular and iconic movies such as Snow White,Star Wars,Toy Story and Pocahontas. Disney also owns and operates several theme parks in the United States, China, Japan, and Europe, including Disneyland, Disney World, DisneySea and Disneyland Paris. The company's diverse portfolio includes animation, live action, comedy, drama, music, sports, and marketing.</p>
+      <img src='./disney/disney.jpg'></img>
+
+
+
+      <h1>About Dora The Explorer</h1>
+      <p>Dora is a popular animated television series that first aired in 2010. It focuses on a young girl named Dora Marjorie Carter, who embarks on various adventures with her talking backpack "Backpack".</p>
+      <img src="./doraexplorer/dora.jpg"></img>
+
+  
+
+      <h1>About Moana</h1>
+      <p>Moana is an animated movie released in 2016 by Walt Disney Animation Studios. The film tells the story of a young Polynesian princess named Moana, who sets sail on a journey to save her island from a curse. Along the way, she encounters various challenges and meets new friends, including a demigod named Maui.</p>
+      <img src='./moanagirl/moana.jpg'></img>
+
+
+      <h1>About Elsa</h1>
+      <p>Elsa is a popular Disney princess who appears in the hit animated movie "Frozen". She is the younger sister of Anna and the daughter of King Agnarr and Queen Iduna. Elsa possesses magical powers that allow her to control the elements, including ice, snow, and water. In the movie, Elsa accidentally uses her powers to create an ice rift, separating her from the rest of the kingdom. Throughout the story, she learns to embrace and control her powers, ultimately helping to save Arendelle from an eternal winter. </p>
+      <img src='./frozen/elsa.avif'></img>
+
+    </div>
+  );
+}
+
+export default App;
